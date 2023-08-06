1eb656cf384846065f0c5bbf180eb6502561740f
hari <harikrishnan9453@gmail.com>
2023-08-06 16:48:13 +0530

    Adding logic to plot player1 and player2 data

--- src/component/TicTacToe/GridComponent/index.js
+++ src/component/TicTacToe/GridComponent/index.js
@@ -3,9 +3,23 @@
 import { useState } from "react";
 import styles from "./index.module.scss";
 const GridComponent = (props) => {
+  const { onChangePlayerData, value, currentPlayer, currentPlayerData } = props;
   const [selectedState, setSelectedState] = useState("");
-  const handleGridSelection = () => {};
-  return <div className={styles.grid}></div>;
+  const handleTabSelection = () => {
+    onChangePlayerData(value);
+  };
+
+  return (
+    <div
+      className={styles.grid}
+      tabIndex={0}
+      role="button"
+      onClick={handleTabSelection}
+    >
+      {currentPlayerData[1]?.includes(value) && <>X</>}
+      {currentPlayerData[2]?.includes(value) && <>O</>}
+    </div>
+  );
 };
 
 export default GridComponent;
--- src/component/TicTacToe/index.js
+++ src/component/TicTacToe/index.js
@@ -1,14 +1,34 @@
+"use client";
+import { useState } from "react";
 import GridComponent from "./GridComponent";
 import styles from "./tictactoe.module.scss";
 const TicTacToe = () => {
-  console.log("Hello");
-
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [currentPlayerData, setCurrentPlayerData] = useState({ 1: [], 2: [] });
+  const [firstPlayer, setFirstPlayer] = useState([]);
+  const [secondPlayer, setSecondPlayer] = useState([]);
+  const onChangePlayerData = (data) => {
+    if (!currentPlayerData[currentPlayer]?.includes(data)) {
+      setCurrentPlayerData({
+        ...currentPlayerData,
+        [currentPlayer]: [...currentPlayerData[currentPlayer], data],
+      });
+      setCurrentPlayer(currentPlayer === 1 ? 2 : 1);
+    }
+  };
   const grid = new Array(9).fill(0);
   return (
     <div>
       <div className={styles.gridRow}>
         {grid.map((item, index) => {
-          return <GridComponent value={index} />;
+          return (
+            <GridComponent
+              value={index}
+              currentPlayer={currentPlayer}
+              currentPlayerData={currentPlayerData}
+              onChangePlayerData={onChangePlayerData}
+            />
+          );
         })}
       </div>
     </div>
