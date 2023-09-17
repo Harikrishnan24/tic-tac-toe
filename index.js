c6d76fa2d7f759245f9d6ce784a1dd2eecb4853a
hari <harikrishnan9453@gmail.com>
2023-09-17 15:00:20 +0530

    Removing import

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { Server } from "socket.io";
-import { FRONTEND_URL } from "./urlConfig";
 const io = new Server(3000, {
   cors: {
     origin: `https://tic-tac-toe-rose-kappa.vercel.app:3001`,
